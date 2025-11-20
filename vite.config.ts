25f380b7c6c51c3c1eb4975c6d23cfdf65611634
Kuilvoer <r.damman@hotmail.com>
2025-11-20 17:38:58 +0100

    fix2

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: './',
+export default defineConfig(({ mode }) => ({
+    base: mode === 'production' ? '/cobblemonmods/' : '/',
     plugins: [
         react(),
         tailwindcss(),
     ],
-})
+}))
